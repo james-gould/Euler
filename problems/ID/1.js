c78b4fc9975a345aaf8b976c5281a08c14499a3e
James Gould <james@csoftware.local>
2017-08-24 14:10:33 +0100

    More docs

--- problems/ID/1.js
+++ problems/ID/1.js
@@ -4,10 +4,12 @@ const Title = "Multiples of 3 and 5";
 
 function Solve() {
 
-  const multiples = Array.from(Array(1000).keys()).filter(item => item % 3 === 0 || item % 5 === 0);
+  // Create an array with items ranging from 0 -> 1000, filter them by != 0 and divisable by 3 or 5.
+  const multiples = Array.from(Array(1000).keys()).filter(item => item != 0 ||  item % 3 === 0 || item % 5 === 0 );
 
   let total = 0;
 
+  // Sum all items in the array
   multiples.forEach((num) => {
     total += num;
   });

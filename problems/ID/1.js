cd178788bb1d93bf278b0a2573daab16b24a2a3b
James Gould <james@csoftware.local>
2017-08-24 13:59:37 +0100

    1 liner, looks messy but works :thinking:

--- problems/ID/1.js
+++ problems/ID/1.js
@@ -3,13 +3,8 @@
 const Title = "Multiples of 3 and 5";
 
 function Solve() {
-  let multiples = [];
 
-  for(let i = 1; i < 1000; i++) {
-    if(i % 3 == 0 || i % 5 == 0) {
-      multiples.push(i);
-    }
-  }
+  const multiples = Array.from(Array(1000).keys()).filter(item => item % 3 === 0 || item % 5 === 0);
 
   let total = 0;
 
---
+++ problems/ID/2.js
@@ -0,0 +1,17 @@
+//https://projecteuler.net/problem=2
+
+const Title = 'Even Fibonacci numbers';
+
+function Solve() {
+  let nextNum, evenNumAddition = 0;
+  let previousNum = 1; // Starting number
+  
+  while(nextNum < 4000000) {
+
+  }
+}
+
+module.exports = {
+  Title,
+  Solve,
+}
